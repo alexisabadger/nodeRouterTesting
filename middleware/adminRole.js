92f1ebbc4e0d858ff7ff285c1ba0b3f50c0fb1d4
alex (is a badger, an IRL badger) <73854333+alexisabadger@users.noreply.github.com>
2022-06-14 17:23:23 -0400

    Update adminRole.js

--- middleware/adminRole.js
+++ middleware/adminRole.js
@@ -1,13 +1,5 @@
-// STEP 7 - Implement Role Verification. The General Steps are:
-//  Check to see if the req.user property exists
-//  if exists: compare the req.user.role to "admin"
-//  if admin: Pass the request to the next handler using next();
-//             
-//  if not: return status 401 "Unauthorized Access"
-//  if not: return status 401 "User is not authenticated"
-
 module.exports = (req, res, next) => {
-  // Your code here
+
   if (req.params) {
     // Check to see if the role is admin
     console.log( JSON.stringify(req.query));
